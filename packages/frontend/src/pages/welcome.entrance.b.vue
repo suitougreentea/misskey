99c7c7e100386bdb883f01f748aace07d7196307
suitougreentea <suitougreentea@users.noreply.github.com>
2023-07-28 15:44:04 +0000

    hidden signup button in simple mode

--- packages/frontend/src/pages/welcome.entrance.b.vue
+++ packages/frontend/src/pages/welcome.entrance.b.vue
@@ -2,18 +2,26 @@
 <div class="rsqzvsbo">
 	<div>This is an alternative entrance screen.</div>
 	<MkButton class="mainAction" full rounded data-cy-signin @click="signin()">{{ i18n.ts.login }}</MkButton>
+	<MkButton class="mainAction hidden" full rounded data-cy-signup @click="signup()">{{ i18n.ts.signup }}</MkButton>
 </div>
 </template>
 
 <script lang="ts" setup>
 import { } from 'vue';
 import XSigninDialog from '@/components/MkSigninDialog.vue';
+import XSignupDialog from '@/components/MkSignupDialog.vue';
 import MkButton from '@/components/MkButton.vue';
 import * as os from '@/os';
 import { i18n } from '@/i18n';
 
 function signin() {
 	os.popup(XSigninDialog, {
+		autoSet: true,
+	}, {}, 'closed');
+}
+
+function signup() {
+	os.popup(XSignupDialog, {
 		autoSet: true,
 	}, {}, 'closed');
 }
@@ -29,6 +37,10 @@ function signin() {
 
 	> .mainAction {
 		line-height: 28px;
+	}
+
+	> .hidden {
+		display: none;
 	}
 }
 </style>
